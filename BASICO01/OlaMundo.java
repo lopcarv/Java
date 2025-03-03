2c39d7b8fee0c10918bad96ee5af77c81faef4f7
lopcarv <d7m7e7@gmail.com>
2025-03-03 14:19:50 -0300

    03mar2025 14:19

---
+++ BASICO01/OlaMundo.java
@@ -0,0 +1,8 @@
+package BASICO01;
+
+public class OlaMundo {
+    public static void main(String[] args) {
+        System.out.println("Olá, Mundo!");
+        System.out.println(" A Versão do Java é: " + System.getProperty("java.version"));
+    }
+}
---
+++ LivroJavaDeitel/README.md
@@ -0,0 +1,1 @@
+olá vamos estudar java ! 
